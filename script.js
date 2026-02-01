1a7b66baaea2d341e8fd70ed1a1bee4566a0ae8e
hype041ph <pedrogamersantos2@gmail.com>
2026-02-01 20:24:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// Lista de mods com links do Google Drive
+// Lista de 10 mods com links do Google Drive (substitua os IDs pelos reais)
 const modsOnline = [
-    { 
-        name: "3 Marias map", 
-        url: "https://drive.google.com/file/d/1KcFQs4moduBMwPZI-HsT0m2f__n9E_uT/view?usp=sharing", 
-        img: "https://i.ytimg.com/vi/oTwzlE348fg/maxresdefault.jpg" 
-    },
-    { 
-        name: "Scania P320 Roll_ONll", 
+    { name: "3 marias farm", url: "https://drive.google.com/file/d/1KcFQs4moduBMwPZI-HsT0m2f__n9E_uT/view?usp=sharing", img: "https://i.ytimg.com/vi/oTwzlE348fg/maxresdefault.jpg" },
+    { name: "Scania P320 Roll_ON", 
         url: "https://drive.google.com/file/d/1zMsuu4w0jkxydoPO0KdVhV6tQrsv292v/view?usp=sharing", 
-        img: "https://files.fs25.net/mods/2025/04/Scania-NTG-P320-Roll-On-1024x576.jpg" 
-    },
-    { 
-        name: "Rancho Fundo", 
+        img: "https://files.fs25.net/mods/2025/04/Scania-NTG-P320-Roll-On-1024x576.jpg" },
+    { name: "Rancho Fundo", 
         url: "https://drive.google.com/file/d/1zuRckvIfBjLW4NpPTf1gyQMP1FjLeMcx/view?usp=sharing", 
-        img: "https://i.ytimg.com/vi/qUFnMlaeD5w/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLArhPjSXRKk9iHfVdH5K-JVH_B8EQ" 
-    }
+        img: "https://i.ytimg.com/vi/qUFnMlaeD5w/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLArhPjSXRKk9iHfVdH5K-JVH_B8EQ" },
+    { name: "Randon Rodotrem Basculante", url: "https://drive.google.com/file/d/1zRNj8awOFlHhtfyzbJuq9GYU4g0xkesy/view?usp=sharing", img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi5sX_iLwmRhuPsgxq18fIlAx5ETDrR7-5ee6RpMwxQjzPudZD2s-YSGu8cipsCDKqBfe03TyxLCYlSP1CxVNKmJKONRy4g5oMeF6SZX_KEesHCc8pxTm-7HRv8zngJWWs5XzoSAhLoc3vcDMp0KUhEPrL9ssdBNuEbiNu9iT3HSq7HOT9EG_tohblf-xPa/w1200-h630-p-k-no-nu/screenshot5.jpg" },
+    { name: "FS25_realGPS", url: "https://drive.google.com/file/d/1iOhkttvubiqTPMQ3HSbCCqFgHZzPOyn_/view?usp=sharing", img: "https://www.kingmods.net/uploads/fs25/mods/real-gps-mod-fs25-dPCap.jpg" },
+    { name: "ToyotaHilux 22 Edit", url: "https://drive.google.com/file/d/1-hKErYMFYk0KOvvu1Bl2evrvC7U6sEPy/view?usp=sharing", img: "https://files.fs25.net/mods/2026/01/Toyota-Hilux-22-Edit-768x432.jpg" },
+    { name: "Store Deliveries", url: "https://drive.google.com/file/d/1Dq1JHf7xODa8ef2rAxB--PwPRxhrmvVg/view?usp=sharing", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIB0qD5HCbCU28pApYbumh0fg0Z3bKzdlC6g&s" },
+    { name: "ShedCowBarn", url: "https://drive.google.com/file/d/1aQcxlaabJgmJLBtwXSdxZdxcps3wtAWd/view?usp=sharing", img: "https://files.fs25.net/mods/2024/11/Shed-Cow-Barn-v1.02.webp" },
+    { name: "ScreenshotMode", url: "https://drive.google.com/file/d/1VQyH3Flx1ZJ2wMZlTHaS2vztLaW4Sof1/view?usp=sharing", img: "https://files.fs25.net/mods/2025/06/Screenshot-Mode.jpg" },
+    { name: "Realistic reFueling System", url: "https://drive.google.com/file/d/16C7PG9ERfDBt-KQ7TGZQi3BOdQRvXrEk/view?usp=sharing", img: "https://files.fs25.net/mods/2025/02/Realistic-refuel-times-v1.0.1.webp" }
 ];
 
 const modList = document.getElementById("modList");
@@ -52,7 +51,11 @@ function renderMods() {
         const btn = document.createElement("button");
         btn.textContent = "Download";
         btn.addEventListener("click", () => {
-            window.open(mod.url, "_blank"); // abre o link do Drive em nova aba
+            if(mod.url && mod.url !== "#"){
+                window.open(mod.url, "_blank");
+            } else {
+                alert(`O mod "${mod.name}" não possui link de download.`);
+            }
         });
         content.appendChild(btn);
 
@@ -61,7 +64,7 @@ function renderMods() {
     });
 }
 
-// Upload funcional (opcional: pode adicionar mods locais à lista, mas downloads Drive ainda funcionam)
+// Upload funcional (adiciona como referência)
 uploadBtn.addEventListener("click", () => {
     const file = modUpload.files[0];
     if(!file){
@@ -69,7 +72,6 @@ uploadBtn.addEventListener("click", () => {
         return;
     }
 
-    // Apenas adiciona à lista como referência (sem link real para Drive)
     modsOnline.push({ 
         name: file.name, 
         url: "#", 
